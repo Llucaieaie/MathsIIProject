e2d6e5326db23a2018b4a07bf97f7ee153aa161f
Lluc <99950000+Llucaieaie@users.noreply.github.com>
2023-01-20 19:12:07 +0100

    reset button

--- citm22_project-main/project.py
+++ citm22_project-main/project.py
@@ -234,7 +234,219 @@ class Arcball(customtkinter.CTk):
         """
         Event triggered function on the event of a push on the button Reset
         """
-        pass
+        rotM = np.array([[1.00,0.00,0.00],
+        [0.00,1.00,0.00],
+        [0.00,0.00,1.00]])
+
+        # Orientation vars. Initialized to represent 0 rotation
+        self.quat = np.array([[1],[0],[0],[0]])
+        self.rotM = np.eye(3)
+        self.AA = {"axis": np.array([[0],[0],[0]]), "angle":0.0}
+        self.rotv = np.array([[0],[0],[0]])
+        self.euler = np.array([[0],[0],[0]])
+
+        # configure window
+        self.title("Holroyd's arcball")
+        self.geometry(f"{1100}x{580}")
+        self.resizable(False, False)
+
+        self.grid_columnconfigure((0,1), weight=0   )
+        self.grid_rowconfigure((0,1), weight=1)
+        self.grid_rowconfigure(2, weight=0)
+
+        # Cube plot
+        self.init_cube()
+
+        self.canvas = FigureCanvasTkAgg(self.fig, self)  # A tk.DrawingArea.
+        self.bm = BlitManager(self.canvas,[self.facesObj])
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(row=0, column=0, rowspan=2, padx=(20, 20), pady=(20, 20), sticky="nsew")
+
+        self.pressed = False #Bool to bypass the information that mouse is clicked
+        self.canvas.mpl_connect('button_press_event', self.onclick)
+        self.canvas.mpl_connect('motion_notify_event', self.onmove)
+        self.canvas.mpl_connect('button_release_event', self.onrelease)
+        
+        # Reset button
+        self.resetbutton = customtkinter.CTkButton(self, text="Reset", command=self.resetbutton_pressed)
+        self.resetbutton.grid(row=3, column=0, padx=(0, 0), pady=(5, 20), sticky="ns")
+        
+        # Selectable atti
+        self.tabview = customtkinter.CTkTabview(self, width=150, height=150)
+        self.tabview.grid(row=0, column=1, padx=(0, 20), pady=(20, 0), sticky="nsew")
+        self.tabview.add("Axis angle")
+        self.tabview.add("Rotation vector")
+        self.tabview.add("Euler angles")
+        self.tabview.add("Quaternion")
+
+        # Selectable atti: AA
+        self.tabview.tab("Axis angle").grid_columnconfigure(0, weight=0)  # configure grid of individual tabs
+        self.tabview.tab("Axis angle").grid_columnconfigure(1, weight=0)  # configure grid of individual tabs
+
+        self.label_AA_axis= customtkinter.CTkLabel(self.tabview.tab("Axis angle"), text="Axis:")
+        self.label_AA_axis.grid(row=0, column=0, rowspan=3, padx=(80,0), pady=(45,0), sticky="e")
+
+        self.entry_AA_ax1 = customtkinter.CTkEntry(self.tabview.tab("Axis angle"))
+        self.entry_AA_ax1.insert(0,"1.0")
+        self.entry_AA_ax1.grid(row=0, column=1, padx=(5, 0), pady=(50, 0), sticky="ew")
+
+        self.entry_AA_ax2 = customtkinter.CTkEntry(self.tabview.tab("Axis angle"))
+        self.entry_AA_ax2.insert(0,"0.0")
+        self.entry_AA_ax2.grid(row=1, column=1, padx=(5, 0), pady=(5, 0), sticky="ew")
+
+        self.entry_AA_ax3 = customtkinter.CTkEntry(self.tabview.tab("Axis angle"))
+        self.entry_AA_ax3.insert(0,"0.0")
+        self.entry_AA_ax3.grid(row=2, column=1, padx=(5, 0), pady=(5, 10), sticky="ew")
+
+        self.label_AA_angle = customtkinter.CTkLabel(self.tabview.tab("Axis angle"), text="Angle:")
+        self.label_AA_angle.grid(row=3, column=0, padx=(120,0), pady=(10, 20),sticky="w")
+        self.entry_AA_angle = customtkinter.CTkEntry(self.tabview.tab("Axis angle"))
+        self.entry_AA_angle.insert(0,"0.0")
+        self.entry_AA_angle.grid(row=3, column=1, padx=(5, 0), pady=(0, 10), sticky="ew")
+
+        self.button_AA = customtkinter.CTkButton(self.tabview.tab("Axis angle"), text="Apply", command=self.apply_AA, width=180)
+        self.button_AA.grid(row=5, column=0, columnspan=2, padx=(0, 0), pady=(5, 0), sticky="e")
+
+        # Selectable atti: rotV
+        self.tabview.tab("Rotation vector").grid_columnconfigure(0, weight=1)
+        self.tabview.tab("Rotation vector").grid_columnconfigure(1, weight=0)
+        
+        self.label_rotV= customtkinter.CTkLabel(self.tabview.tab("Rotation vector"), text="rot. Vector:")
+        self.label_rotV.grid(row=0, column=0, rowspan=3, padx=(2,0), pady=(45,0), sticky="e")
+
+        self.entry_rotV_1 = customtkinter.CTkEntry(self.tabview.tab("Rotation vector"))
+        self.entry_rotV_1.insert(0,"0.0")
+        self.entry_rotV_1.grid(row=0, column=1, padx=(5, 60), pady=(50, 0), sticky="ew")
+
+        self.entry_rotV_2 = customtkinter.CTkEntry(self.tabview.tab("Rotation vector"))
+        self.entry_rotV_2.insert(0,"0.0")
+        self.entry_rotV_2.grid(row=1, column=1, padx=(5, 60), pady=(5, 0), sticky="ew")
+
+        self.entry_rotV_3 = customtkinter.CTkEntry(self.tabview.tab("Rotation vector"))
+        self.entry_rotV_3.insert(0,"0.0")
+        self.entry_rotV_3.grid(row=2, column=1, padx=(5, 60), pady=(5, 10), sticky="ew")
+
+        self.button_rotV = customtkinter.CTkButton(self.tabview.tab("Rotation vector"), text="Apply", command=self.apply_rotV, width=180)
+        self.button_rotV.grid(row=5, column=0, columnspan=2, padx=(0, 60), pady=(5, 0), sticky="e")
+
+        # Selectable atti: Euler angles
+        self.tabview.tab("Euler angles").grid_columnconfigure(0, weight=1)
+        self.tabview.tab("Euler angles").grid_columnconfigure(1, weight=0)
+        
+        self.label_EA_roll= customtkinter.CTkLabel(self.tabview.tab("Euler angles"), text="roll:")
+        self.label_EA_roll.grid(row=0, column=0, padx=(2,0), pady=(50,0), sticky="e")
+
+        self.label_EA_pitch= customtkinter.CTkLabel(self.tabview.tab("Euler angles"), text="pitch:")
+        self.label_EA_pitch.grid(row=1, column=0, padx=(2,0), pady=(5,0), sticky="e")
+
+        self.label_EA_yaw= customtkinter.CTkLabel(self.tabview.tab("Euler angles"), text="yaw:")
+        self.label_EA_yaw.grid(row=2, column=0, rowspan=3, padx=(2,0), pady=(5,10), sticky="e")
+
+        self.entry_EA_roll = customtkinter.CTkEntry(self.tabview.tab("Euler angles"))
+        self.entry_EA_roll.insert(0,"0.0")
+        self.entry_EA_roll.grid(row=0, column=1, padx=(5, 60), pady=(50, 0), sticky="ew")
+
+        self.entry_EA_pitch = customtkinter.CTkEntry(self.tabview.tab("Euler angles"))
+        self.entry_EA_pitch.insert(0,"0.0")
+        self.entry_EA_pitch.grid(row=1, column=1, padx=(5, 60), pady=(5, 0), sticky="ew")
+
+        self.entry_EA_yaw = customtkinter.CTkEntry(self.tabview.tab("Euler angles"))
+        self.entry_EA_yaw.insert(0,"0.0")
+        self.entry_EA_yaw.grid(row=2, column=1, padx=(5, 60), pady=(5, 10), sticky="ew")
+
+        self.button_EA = customtkinter.CTkButton(self.tabview.tab("Euler angles"), text="Apply", command=self.apply_EA, width=180)
+        self.button_EA.grid(row=5, column=0, columnspan=2, padx=(0, 60), pady=(5, 0), sticky="e")
+
+        # Selectable atti: Quaternion
+        self.tabview.tab("Quaternion").grid_columnconfigure(0, weight=1)
+        self.tabview.tab("Quaternion").grid_columnconfigure(1, weight=0)
+        
+        self.label_quat_0= customtkinter.CTkLabel(self.tabview.tab("Quaternion"), text="q0:")
+        self.label_quat_0.grid(row=0, column=0, padx=(2,0), pady=(50,0), sticky="e")
+
+        self.label_quat_1= customtkinter.CTkLabel(self.tabview.tab("Quaternion"), text="q1:")
+        self.label_quat_1.grid(row=1, column=0, padx=(2,0), pady=(5,0), sticky="e")
+
+        self.label_quat_2= customtkinter.CTkLabel(self.tabview.tab("Quaternion"), text="q2:")
+        self.label_quat_2.grid(row=2, column=0, padx=(2,0), pady=(5,0), sticky="e")
+
+        self.label_quat_3= customtkinter.CTkLabel(self.tabview.tab("Quaternion"), text="q3:")
+        self.label_quat_3.grid(row=3, column=0, padx=(2,0), pady=(5,10), sticky="e")
+
+        self.entry_quat_0 = customtkinter.CTkEntry(self.tabview.tab("Quaternion"))
+        self.entry_quat_0.insert(0,"1.0")
+        self.entry_quat_0.grid(row=0, column=1, padx=(5, 60), pady=(50, 0), sticky="ew")
+
+        self.entry_quat_1 = customtkinter.CTkEntry(self.tabview.tab("Quaternion"))
+        self.entry_quat_1.insert(0,"0.0")
+        self.entry_quat_1.grid(row=1, column=1, padx=(5, 60), pady=(5, 0), sticky="ew")
+
+        self.entry_quat_2 = customtkinter.CTkEntry(self.tabview.tab("Quaternion"))
+        self.entry_quat_2.insert(0,"0.0")
+        self.entry_quat_2.grid(row=2, column=1, padx=(5, 60), pady=(5, 0), sticky="ew")
+
+        self.entry_quat_3 = customtkinter.CTkEntry(self.tabview.tab("Quaternion"))
+        self.entry_quat_3.insert(0,"0.0")
+        self.entry_quat_3.grid(row=3, column=1, padx=(5, 60), pady=(5, 10), sticky="ew")
+
+        self.button_quat = customtkinter.CTkButton(self.tabview.tab("Quaternion"), text="Apply", command=self.apply_quat, width=180)
+        self.button_quat.grid(row=4, column=0, columnspan=2, padx=(0, 60), pady=(5, 0), sticky="e")
+
+        # Rotation matrix info
+        self.RotMFrame = customtkinter.CTkFrame(self, width=150)
+        self.RotMFrame.grid(row=1, column=1, rowspan=3, padx=(0, 20), pady=(20, 20), sticky="nsew")
+
+        self.RotMFrame.grid_columnconfigure((0,1,2,3,4), weight=1)
+
+        self.label_RotM= customtkinter.CTkLabel(self.RotMFrame, text="RotM = ")
+        self.label_RotM.grid(row=0, column=0, rowspan=3, padx=(2,0), pady=(20,0), sticky="e")
+
+        self.entry_RotM_11= customtkinter.CTkEntry(self.RotMFrame, width=50, border_width=0)
+        self.entry_RotM_11.insert(0,rotM[0,0])
+        self.entry_RotM_11.configure(state="disabled")
+        self.entry_RotM_11.grid(row=0, column=1, padx=(2,0), pady=(20,0), sticky="ew")
+
+
+        self.entry_RotM_12= customtkinter.CTkEntry(self.RotMFrame, width=50, border_width=0)
+        self.entry_RotM_12.insert(0,rotM[0,1])
+        self.entry_RotM_12.configure(state="disabled")
+        self.entry_RotM_12.grid(row=0, column=2, padx=(2,0), pady=(20,0), sticky="ew")
+
+        self.entry_RotM_13= customtkinter.CTkEntry(self.RotMFrame, width=50, border_width=0)
+        self.entry_RotM_13.insert(0,rotM[0,2])
+        self.entry_RotM_13.configure(state="disabled")
+        self.entry_RotM_13.grid(row=0, column=3, padx=(2,0), pady=(20,0), sticky="ew")
+
+        self.entry_RotM_21= customtkinter.CTkEntry(self.RotMFrame, width=50, border_width=0)
+        self.entry_RotM_21.insert(0,rotM[1,0])
+        self.entry_RotM_21.configure(state="disabled")
+        self.entry_RotM_21.grid(row=1, column=1, padx=(2,0), pady=(2,0), sticky="ew")
+
+        self.entry_RotM_22= customtkinter.CTkEntry(self.RotMFrame, width=50, border_width=0)
+        self.entry_RotM_22.insert(0,rotM[1,1])
+        self.entry_RotM_22.configure(state="disabled")
+        self.entry_RotM_22.grid(row=1, column=2, padx=(2,0), pady=(2,0), sticky="ew")
+
+        self.entry_RotM_23= customtkinter.CTkEntry(self.RotMFrame, width=50, border_width=0)
+        self.entry_RotM_23.insert(0,rotM[1,2])
+        self.entry_RotM_23.configure(state="disabled")
+        self.entry_RotM_23.grid(row=1, column=3, padx=(2,0), pady=(2,0), sticky="ew")
+
+        self.entry_RotM_31= customtkinter.CTkEntry(self.RotMFrame, width=50, border_width=0)
+        self.entry_RotM_31.insert(0,rotM[2,0])
+        self.entry_RotM_31.configure(state="disabled")
+        self.entry_RotM_31.grid(row=2, column=1, padx=(2,0), pady=(2,0), sticky="ew")
+
+        self.entry_RotM_32= customtkinter.CTkEntry(self.RotMFrame, width=50, border_width=0)
+        self.entry_RotM_32.insert(0,rotM[2,1])
+        self.entry_RotM_32.configure(state="disabled")
+        self.entry_RotM_32.grid(row=2, column=2, padx=(2,0), pady=(2,0), sticky="ew")
+
+        self.entry_RotM_33= customtkinter.CTkEntry(self.RotMFrame, width=50, border_width=0)
+        self.entry_RotM_33.insert(0,rotM[2,2])
+        self.entry_RotM_33.configure(state="disabled")
+        self.entry_RotM_33.grid(row=2, column=3, padx=(2,0), pady=(2,0), sticky="ew")
+        print("reset")
 
     
     def apply_AA(self):
